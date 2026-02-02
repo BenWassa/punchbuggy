31635e6fe826465a6e77e88f4150f7704b8251db
BenWassa <haddonbenjamin@gmail.com>
2026-02-01 22:59:42 -0500

    chore: release 3.1.0

--- docs/app-version.js
+++ docs/app-version.js
@@ -1,4 +1,4 @@
 // Application version used by the service worker and UI.
 // Application version used by the service worker and UI.
 // Major release: UI refresh, remove backups/migrations, silent updates.
-self.PUNCHBUGGY_APP_VERSION = "3.0.0";
+self.PUNCHBUGGY_APP_VERSION = "3.1.0";
